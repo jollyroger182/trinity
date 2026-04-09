2767d5c411c28aad39f8706aef6b5511fdc41c61
jollyroger182 <hello@jollyy.dev>
2026-04-09 21:29:16 +0800

    feat: set up postgres database

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,4 +1,6 @@
-import { drizzle } from 'drizzle-orm/d1'
+import { drizzle } from 'drizzle-orm/node-postgres'
 import * as schema from './schema'
 
-export const getDb = (d1: D1Database) => drizzle(d1, { schema })
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL env var not set')
+
+export const db = drizzle(process.env.DATABASE_URL, { schema })
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,5 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { pgTable, text } from 'drizzle-orm/pg-core'
 
-export const task = sqliteTable('task', {
-	id: text('id')
-		.primaryKey()
-		.$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1),
+export const users = pgTable('users', {
+	id: text('id').primaryKey(),
 })

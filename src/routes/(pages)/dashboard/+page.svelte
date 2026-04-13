fada50d67b3967ae8c508a42c917f28a83248e51
jollyroger182 <hello@jollyy.dev>
2026-04-13 14:45:42 +0800

    feat: move create project card below the heading

--- src/routes/(pages)/dashboard/+page.svelte
+++ src/routes/(pages)/dashboard/+page.svelte
@@ -15,6 +15,9 @@
 		projects.push(project)
 		console.log(projects)
 		creating = false
+		createState.name = ''
+		createState.description = ''
+		createState.hackatimeProjects = []
 	}
 </script>
 
@@ -26,27 +29,26 @@
 
 <h2 style="display: flex; align-items: center; flex-wrap: wrap">
 	<span style="flex: 1 0 0">Your projects</span>
-	{#if creating}
-		<div class="form-card" in:slide={{ duration: 200 }}>
-			<input placeholder="Name" bind:value={createState.name} />
-			<textarea placeholder="Description" bind:value={createState.description}></textarea>
-			<select multiple bind:value={createState.hackatimeProjects}>
-				<option value="example">example</option>
-				<option value="placeholder">placeholder</option>
-			</select>
-			<div style="">
-				<button class="btn-hover-slide" onclick={submitCreate}><span>Submit</span></button>
-				<button class="btn-hover-slide" onclick={() => (creating = false)}
-					><span>Cancel</span></button
-				>
-			</div>
-		</div>
-	{:else}
-		<button class="btn-skewed btn-hover-slide" onclick={() => (creating = true)}
-			><span>➕ Create</span></button
-		>
-	{/if}
+	<button class="btn-skewed btn-hover-slide" onclick={() => (creating = !creating)}
+		><span>➕ Create</span></button
+	>
 </h2>
+
+{#if creating}
+	<div class="form-card" style="margin-bottom: 1em" transition:slide={{ duration: 200 }}>
+		<input placeholder="Name" bind:value={createState.name} />
+		<textarea placeholder="Description" bind:value={createState.description}></textarea>
+		<select multiple bind:value={createState.hackatimeProjects}>
+			<option value="example">example</option>
+			<option value="placeholder">placeholder</option>
+		</select>
+		<div style="">
+			<button class="btn-hover-slide" onclick={submitCreate}><span>Submit</span></button>
+			<button class="btn-hover-slide" onclick={() => (creating = false)}><span>Cancel</span></button
+			>
+		</div>
+	</div>
+{/if}
 
 {#each projects as project (project.id)}
 	<div style="border: 3px solid var(--color-fg); padding: 0 1em; margin-bottom: 1em">
@@ -58,6 +60,10 @@
 		<p>{project.description}</p>
 	</div>
 {/each}
+
+{#if projects.length === 0}
+	You don't have any projects. Click the button to create one!
+{/if}
 
 <style>
 	.form-card {
